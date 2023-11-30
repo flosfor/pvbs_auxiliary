0f28186f916e25d1fe7d33c0bafcbace91e865ca
flosfor <36123627+flosfor@users.noreply.github.com>
2023-11-30 12:07:07 -0500

    Add files via upload

--- temp_extractintrinsic.m
+++ temp_extractintrinsic.m
@@ -24,6 +24,26 @@ for i = 1:length(intrinsics)
     rinAll(end + 1) = rinTemp;
 end
 
+sagAll = [];
+sagPC = [];
+sagFS = [];
+sagNFS = [];
+for i = 1:length(intrinsics)
+    sagTemp = intrinsics{i};
+    sagTemp = sagTemp.sag_ratio;
+    sagAll(end + 1) = sagTemp;
+end
+
+rheoAll = [];
+rheoPC = [];
+rheoFS = [];
+rheoNFS = [];
+for i = 1:length(intrinsics)
+    rheoTemp = intrinsics{i};
+    rheoTemp = rheoTemp.rheobase;
+    rheoAll(end + 1) = rheoTemp;
+end
+
 % manually designate indices for cell types
 %{
 for i = idxPC
@@ -63,6 +83,44 @@ end
 mean_rinPC = mean(rinPC);
 mean_rinFS = mean(rinFS);
 mean_rinNFS = mean(rinNFS);
+
+for i = idxPC
+    sagTemp = intrinsics{i};
+    sagTemp = sagTemp.sag_ratio;
+    sagPC(end + 1) = sagTemp;
+end
+for i = idxFS
+    sagTemp = intrinsics{i};
+    sagTemp = sagTemp.sag_ratio;
+    sagFS(end + 1) = sagTemp;
+end
+for i = idxNFS
+    sagTemp = intrinsics{i};
+    sagTemp = sagTemp.sag_ratio;
+    sagNFS(end + 1) = sagTemp;
+end
+mean_sagPC = mean(sagPC);
+mean_sagFS = mean(sagFS);
+mean_sagNFS = mean(sagNFS);
+
+for i = idxPC
+    rheoTemp = intrinsics{i};
+    rheoTemp = rheoTemp.rheobase;
+    rheoPC(end + 1) = rheoTemp;
+end
+for i = idxFS
+    rheoTemp = intrinsics{i};
+    rheoTemp = rheoTemp.rheobase;
+    rheoFS(end + 1) = rheoTemp;
+end
+for i = idxNFS
+    rheoTemp = intrinsics{i};
+    rheoTemp = rheoTemp.rheobase;
+    rheoNFS(end + 1) = rheoTemp;
+end
+mean_rheoPC = mean(rheoPC);
+mean_rheoFS = mean(rheoFS);
+mean_rheoNFS = mean(rheoNFS);
 %}
 
 %%{
@@ -70,8 +128,19 @@ rmpAll = rmpAll';
 rmpPC = rmpPC';
 rmpFS = rmpFS';
 rmpNFS = rmpNFS';
+
 rinAll = rinAll';
 rinPC = rinPC';
 rinFS = rinFS';
 rinNFS = rinNFS';
+
+sagAll = sagAll';
+sagPC = sagPC';
+sagFS = sagFS';
+sagNFS = sagNFS';
+
+rheoAll = rheoAll';
+rheoPC = rheoPC';
+rheoFS = rheoFS';
+rheoNFS = rheoNFS';
 %}
